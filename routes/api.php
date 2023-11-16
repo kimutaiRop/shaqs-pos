940916c1f268bf56a9da4c1bef622321281a5ff0
kimutai Rop <kimrop19@gmail.com>
2023-11-16 13:32:23 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,35 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// api route for order
+Route::get('/orders', 'App\Http\Controllers\OrderController@index');
+Route::post('/orders', 'App\Http\Controllers\OrderController@store');
+Route::put('/orders/{id}', 'App\Http\Controllers\OrderController@update');
+Route::delete('/orders/{id}', 'App\Http\Controllers\OrderController@destroy');
+
+// api route for payment
+Route::get('/payments', 'App\Http\Controllers\PaymentController@index');
+Route::post('/payments', 'App\Http\Controllers\PaymentController@store');
+Route::put('/payments/{id}', 'App\Http\Controllers\PaymentController@update');
+Route::delete('/payments/{id}', 'App\Http\Controllers\PaymentController@destroy');
+
+// api route for menu
+Route::get('/menu', 'App\Http\Controllers\MenuController@index');
+Route::post('/menu', 'App\Http\Controllers\MenuController@store');
+Route::put('/menu/{id}', 'App\Http\Controllers\MenuController@update');
+Route::delete('/menu/{id}', 'App\Http\Controllers\MenuController@destroy');
+
+// api route for user76
+Route::get('/users', 'App\Http\Controllers\UserController@index');
+Route::post('/users', 'App\Http\Controllers\UserController@store');
+Route::put('/users/{id}', 'App\Http\Controllers\UserController@update');
+Route::delete('/users/{id}', 'App\Http\Controllers\UserController@destroy');
+
+
+// menu category
+Route::get('/menu-category', 'App\Http\Controllers\MenuCategoryController@index');
+Route::post('/menu-category', 'App\Http\Controllers\MenuCategoryController@store');
+Route::put('/menu-category/{id}', 'App\Http\Controllers\MenuCategoryController@update');
+Route::delete('/menu-category/{id}', 'App\Http\Controllers\MenuCategoryController@destroy');
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+// Route::get('/menu', function () {
+//     return view('menu');
+// });
+
+// Route::get('/orders', function () {
+//     return view('orders');
+// });
+
+// Route::get('/payments', function () {
+//     return view('payments');
+// });
+
+
+// Route::inertia('/example', 'Example');
+
+Route::get("/", function(){
+    return Inertia::render("Home");
+});
+
+Route::get("/payments", function(){
+    return Inertia::render("Payments");
+});
+Route::get("/menu", function(){
+    return Inertia::render("Menu");
+});
+Route::get("/orders", function(){
+    return Inertia::render("Orders");
+});
+Route::get("/login", function(){
+    return Inertia::render("Login");
 });
